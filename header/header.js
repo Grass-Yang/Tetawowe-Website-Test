a951a126eb3b08a0f01403e39f53472e89da316e
Grass-Yang <zhuya16.forever@gmail.com>
2025-10-02 17:39:01 +0800

    Update header.js

--- header/header.js
+++ header/header.js
@@ -1,11 +1,12 @@
 const headerContainer = document.getElementById('header-placeholder');
 
-fetch('../header/header.html')
+fetch('/header/header.html')
   .then(response => response.text())
   .then(data => {
     headerContainer.innerHTML = data;
   })
   .catch(err => console.error('Failed to load header.html:', err));
+
 
 
 
